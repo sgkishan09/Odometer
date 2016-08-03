a71b0d2a06b31ec85d97916a9805af586f849501
gkishan <gkishan@L-MAA-25005033.paypalcorp.com>
2016-08-03 17:45:01 +0530

    Initial Template

---
+++ Odometer/src/Odometer.java
@@ -0,0 +1,36 @@
+
+public class Odometer {
+	public static void main(String[] args)
+	{
+		
+	}
+	public static boolean isValid(int input)
+	{
+
+	}
+	public static int generateNext(int input)
+	{
+		
+	}
+	public static int generatePrevious(int input)
+	{
+		
+	}
+	public static int generateLowest(int digits)
+	{
+		
+	}
+	public static int generateNth(int initial,int N)
+	{
+		
+	}
+	public static int difference(int start,int end)
+	{
+		
+	}
+	public static int largest(int N)
+	{
+		
+	}
+	
+}
