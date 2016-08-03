d6f3ef64d014bfcd72aede44abd9a15ca1018d7f
gkishan <gkishan@L-MAA-25005033.paypalcorp.com>
2016-08-03 18:04:42 +0530

    Added TestCases

--- Odometer/src/Odometer.java
+++ Odometer/src/Odometer.java
@@ -1,36 +1,70 @@
-
 public class Odometer {
-	public static void main(String[] args)
-	{
-		
+	public static void main(String[] args) {
+		runTests();
 	}
-	public static boolean isValid(int input)
+	public static void runTests()
 	{
+		testisValid();
+		testgenerateLargest();
+		testdifference();
+		testgenerateLowest();
+		testgenerateNext();
+		testgeneratePrevious();
+		testgenerateNth();
+	}
+	public static void testisValid() {
+		assert isValid(1234) == true;
+		assert isValid(1233) == false;
+	}
+
+	public static void testgenerateNext() {
+		assert generateNext(1234) == 1235;
+	}
+
+	public static void testgeneratePrevious() {
+		assert generatePrevious(1235) == 1234;
+	}
+
+	public static void testgenerateLowest() {
+		assert generateLowest(4) == 1234;
+	}
+
+	public static void testgenerateNth() {
+		assert generateNth(1234, 2) == 1236;
+	}
+
+	public static void testdifference() {
+		assert difference(1234, 1236) == 2;
+	}
+	public static void testgenerateLargest(){
+		assert generateLargest(4)==6789;
+	}
+	public static boolean isValid(int input) {
 
 	}
-	public static int generateNext(int input)
-	{
-		
+
+	public static int generateNext(int input) {
+
 	}
-	public static int generatePrevious(int input)
-	{
-		
+
+	public static int generatePrevious(int input) {
+
 	}
-	public static int generateLowest(int digits)
-	{
-		
+
+	public static int generateLowest(int digits) {
+
 	}
-	public static int generateNth(int initial,int N)
-	{
-		
+
+	public static int generateNth(int initial, int N) {
+
 	}
-	public static int difference(int start,int end)
-	{
-		
+
+	public static int difference(int start, int end) {
+
 	}
-	public static int largest(int N)
-	{
-		
+
+	public static int generateLargest(int N) {
+
 	}
-	
+
 }
