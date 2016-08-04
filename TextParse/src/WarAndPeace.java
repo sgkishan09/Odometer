f8bb1aacfa0591aabf092021128bb00ec7b8c8a0
gkishan <gkishan@L-MAA-25005033.paypalcorp.com>
2016-08-04 12:07:44 +0530

    Anagram Clusters

--- TextParse/src/WarAndPeace.java
+++ TextParse/src/WarAndPeace.java
@@ -1,29 +1,47 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class WarAndPeace {
 	public static void main(String[] args) {
-		final String path = "C:\\Projects\\WarAndPeace.txt";
-		final int N = 10;
-		Map<String, Integer> frequencyMap = countWords(tokenizer(readFile(path)));
-		printMap(mostFrequentWords(frequencyMap, N), frequencyMap);
-	}
-	public static void printMap(List<String> words, Map<String, Integer> frequencyMap) {
-
-	}
-
-	public static String readFile(String path) {
-		return "";
+		String filePath = "C:\\Projects\\sowpods.txt";
+		try {
+			printMap(generateCluster(readFile(filePath)));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
-	public static Map<String, Integer> countWords(LinkedList<String> tokens) {
-		return null;
+	public static String sortByChar(String word) {
+		char[] chars = word.toCharArray();
+		Arrays.sort(chars);
+		return new String(chars);
 	}
 
-	public static LinkedList<String> tokenizer(String input) {
-		return null;
+	public static Map<String, List<String>> generateCluster(List<String> words) {
+		Map<String, List<String>> clusters = new HashMap<>();
+		for (String word : words) {
+			String sortedWord = sortByChar(word);
+			if (clusters.containsKey(sortedWord))
+				clusters.get(sortedWord).add(word);
+			else {
+				ArrayList<String> cluster = new ArrayList<>();
+				cluster.add(word);
+				clusters.put(sortedWord, cluster);
+			}
+		}
+		return clusters;
 	}
 
-	public static List<String> mostFrequentWords(Map<String, Integer> frequencyMap, int N) {
-		return null;
+	public static void printMap(Map<String, List<String>> clusterMap) {
+		for (String token : clusterMap.keySet())
+			if (clusterMap.get(token).size() > 1)
+				System.out.println(clusterMap.get(token));
+	}
+
+	public static ArrayList<String> readFile(String path) throws Exception {
+		return (ArrayList<String>) Files.readAllLines(Paths.get(path));
 	}
 }
